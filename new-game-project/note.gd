3ce463f6ff38a86aabf0cf5bb6102bb0f6173b03
demo-argenti <91711506+demo-argenti@users.noreply.github.com>
2025-02-09 23:13:03 -0500

    Some minor tweaks
    Just changing some things and testing them

--- new-game-project/note.gd
+++ new-game-project/note.gd
@@ -1,15 +1,12 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
 @onready var _animation_player = $AnimationPlayer
 @onready var _hit_area = $HitArea
+@export  var input_key: String
 
 func _physics_process(delta: float) -> void:
 	_animation_player.play("move_note_normal")
-	if Input.is_action_just_pressed("ui_up") and _hit_area.overlaps_area($"../NoteChecker"):
+	if Input.is_action_just_pressed(input_key) and _hit_area.overlaps_area($"../NoteChecker"):
 		die()
 	
 func die():
